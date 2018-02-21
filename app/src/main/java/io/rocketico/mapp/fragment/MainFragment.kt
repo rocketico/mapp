e67c5433577b5eb83529b4bfd4e26bee183a0b32
KirillZholnerovich <danielcooverw@gmail.com>
2018-02-21 18:46:26 +0300

    Some changes

--- app/src/main/java/io/rocketico/mapp/fragment/MainFragment.kt
+++ app/src/main/java/io/rocketico/mapp/fragment/MainFragment.kt
@@ -1,11 +1,14 @@
 package io.rocketico.mapp.fragment
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.rocketico.mapp.R
+import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.header_main.*
 
 class MainFragment : Fragment() {
@@ -23,9 +26,26 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        viewPager.adapter = object : FragmentStatePagerAdapter(activity.supportFragmentManager) {
+            override fun getItem(position: Int): Fragment = when(position) {
+                0 -> StatisticsFragment()
+                1 -> HistoryFragment()
+                else -> throw IllegalArgumentException()
+            }
+
+            override fun getCount(): Int = 2
+        }
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
         menuImageButton.setOnClickListener {
             mainFragmentListener.onMenuButtonClick()
         }
+
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+        tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(viewPager))
     }
 
     interface MainFragmentListener {
---
+++ app/src/main/java/io/rocketico/mapp/fragment/StatisticsFragment.kt
@@ -0,0 +1,16 @@
+package io.rocketico.mapp.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.rocketico.mapp.R
+
+class StatisticsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_statistics, container, false)
+    }
+
+}
