63d79a2f1147787ef2670c861ef5ace9830850ec
KirillZholnerovich <danielcooverw@gmail.com>
2018-03-21 21:36:51 +0300

    Fixed eth node address

--- app/src/main/java/io/rocketico/mapp/Cc.kt
+++ app/src/main/java/io/rocketico/mapp/Cc.kt
@@ -3,5 +3,5 @@ package io.rocketico.mapp
 //Constants
 object Cc {
     const val WALLET_NAME = "main_wallet"
-    const val ETH_NODE = "https://infura.io/fd28b54b765d41c8d352d092576bb125"
+    const val ETH_NODE = "https://mainnet.infura.io/fd28b54b765d41c8d352d092576bb125"
 }
