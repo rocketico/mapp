e350a35b1d0f402844816a5508a9947638b1eb1a
KirillZholnerovich <danielcooverw@gmail.com>
2018-03-30 18:28:04 +0300

    Changed API

--- core/src/main/java/io/rocketico/core/api/Api.kt
+++ core/src/main/java/io/rocketico/core/api/Api.kt
@@ -10,23 +10,23 @@ import retrofit2.http.Query
 import java.util.*
 
 interface Api {
-    @GET("https://gist.githubusercontent.com/KirillZholnerovich/c8f50d91479599e2ce79cd773e7c16e1/raw/0b9c708ecc61edf0e225200ce2fdaa6ceecae0da/gistfile1.txt")
+    @GET("api/rates-by-date")
     fun getRatesByDate(
             @Query("date") date: Date
     ): Call<TokensRatesResponse>
 
-    @GET("https://gist.githubusercontent.com/uxname/bd779159538c914ab8563241d5c37858/raw/acd09e0b05f025444578af807370001e79b99d09/gistfile1.txt")
+    @GET("api/rates-by-range")
     fun getRatesByDateRange(
             @Query("fromDate") fromDate: Date,
             @Query("toDate") toDate: Date
     ): Call<TokenRatesRangeResponse>
 
-    @GET("https://gist.githubusercontent.com/KirillZholnerovich/2223525229c2de361bb37f347b5c33d9/raw/244b406df3a537abe882f14dac79277858d19bbe/gistfile1.txt")
+    @GET("api/token/info")
     fun getTokenInfo(@Query("tokenType") tokenType: String,
                      @Query("currency") currency: String
     ): Call<TokenInfoResponse>
 
-    @GET("https://gist.githubusercontent.com/uxname/1c6689cac6b8bb69e39ed4aeeb8280de/raw/435216688cbe9430959bc91cafffe374cf63feb9/gistfile1.txt")
+    @GET("api/transactions")
     fun getTokensHistory(
             @Query("tokenTypeList") tokenTypeList: List<String>,
             @Query("dateFrom") dateFrom: Date,
