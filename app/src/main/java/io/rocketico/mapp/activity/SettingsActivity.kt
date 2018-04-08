d8daf609b12700773bcf8c0ddc14fa20eb8bcf53
KirillZholnerovich <danielcooverw@gmail.com>
2018-04-08 14:54:58 +0300

    Added logout logic

--- app/src/main/java/io/rocketico/mapp/activity/SettingsActivity.kt
+++ app/src/main/java/io/rocketico/mapp/activity/SettingsActivity.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.afollestad.materialdialogs.MaterialDialog
+import io.rocketico.core.BalanceHelper
+import io.rocketico.core.RateHelper
 import io.rocketico.core.WalletManager
 import io.rocketico.mapp.R
 import io.rocketico.mapp.Utils
@@ -31,6 +34,25 @@ class SettingsActivity : AppCompatActivity(),
     override fun onExportClick() {
         val wallet = WalletManager(this).getWallet()!!
         startActivity(Utils.shareTextIntent(wallet.privateKey))
+    }
+
+    override fun onLogoutClick() {
+        val dialog = MaterialDialog.Builder(this)
+                .title(getString(R.string.logout))
+                .content(getString(R.string.logout_content))
+                .positiveText(getString(R.string.yes))
+                .negativeText(getString(R.string.no))
+                .onPositive { dialog, which ->
+                    RateHelper.deleteAllRates(this)
+                    BalanceHelper.deleteAllBalances(this)
+                    WalletManager(this).deleteWallet()
+
+                    val intent = MainActivity.newIntent(this)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    startActivity(intent)
+                    finish()
+                }
+                .show()
     }
 
     companion object {
