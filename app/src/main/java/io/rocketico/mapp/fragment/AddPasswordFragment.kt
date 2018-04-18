abdf10d1dacb8f794c8d19b83a89e8c8b725dc9f
KirillZholnerovich <danielcooverw@gmail.com>
2018-04-18 17:30:20 +0300

    Changed password creation logic

--- app/src/main/java/io/rocketico/mapp/fragment/AddPasswordFragment.kt
+++ app/src/main/java/io/rocketico/mapp/fragment/AddPasswordFragment.kt
@@ -1,16 +1,23 @@
 package io.rocketico.mapp.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import io.rocketico.mapp.R
 import kotlinx.android.synthetic.main.fragment_add_password.*
 import org.jetbrains.anko.toast
 
 class AddPasswordFragment : Fragment() {
+
     private lateinit var fragmentListener: AddPasswordFragmentListener
+    private var password: String = ""
+    private var confirmPassword: String = ""
+
+    private var isPasswordEntered: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +30,38 @@ class AddPasswordFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-       buttonBack.setOnClickListener {
-           fragmentListener.onBackClick()
-       }
+        focusPassword()
 
-       createPasswordButton.setOnClickListener {
-           if (passwordEditText.text.toString() == confirmPasswordEditText.text.toString()){
-               fragmentListener.onCreateClick(passwordEditText.text.toString())
-           } else {
-               context!!.toast(getString(R.string.no_match))
-           }
-       }
+        buttonBack.setOnClickListener {
+            fragmentListener.onBackClick()
+        }
+
+        buttonCheck.setOnClickListener {
+            if (!isPasswordEntered) {
+                password = passwordEditText.text.toString()
+                isPasswordEntered = true
+
+                enterPasswordTextView.text = getString(R.string.confirm_password_label)
+                passwordEditText.text.clear()
+
+                focusPassword()
+            } else {
+                confirmPassword = passwordEditText.text.toString()
+                if (password == confirmPassword) {
+                    fragmentListener.onCreateClick(password)
+                } else {
+                    context!!.toast(getString(R.string.no_match))
+                }
+            }
+        }
+    }
+
+    private fun focusPassword() {
+        passwordEditText.postDelayed({
+            passwordEditText.requestFocus()
+            val imm = context!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(passwordEditText, InputMethodManager.SHOW_IMPLICIT)
+        }, 50)
     }
 
     interface AddPasswordFragmentListener {
