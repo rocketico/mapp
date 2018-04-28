ef6c2389afe2bd27529ba0050ddc6c4e99ab101a
KirillZholnerovich <danielcooverw@gmail.com>
2018-04-28 16:45:20 +0300

    Fixed direction icon in header

--- app/src/main/java/io/rocketico/mapp/fragment/MainFragment.kt
+++ app/src/main/java/io/rocketico/mapp/fragment/MainFragment.kt
@@ -328,11 +328,13 @@ class MainFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
                     directionHeader.setImageDrawable(resources.getDrawable(R.drawable.ic_direction_down))
                     directionHeader.setColorFilter(resources.getColor(R.color.colorAccent))
                     percentDiffTextView.setTextColor(resources.getColor(R.color.colorAccent))
+                    directionHeader.visibility = View.VISIBLE
                 }
                 percentDiff >= 0.01f -> {
                     directionHeader.setImageDrawable(resources.getDrawable(R.drawable.ic_direction_up))
                     directionHeader.setColorFilter(resources.getColor(R.color.joinColor))
                     percentDiffTextView.setTextColor(resources.getColor(R.color.joinColor))
+                    directionHeader.visibility = View.VISIBLE
                 }
                 else -> {
                     directionHeader.visibility = View.GONE
