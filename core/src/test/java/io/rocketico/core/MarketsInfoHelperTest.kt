becff9bad175040b0c00a08b13e7254f5e03b04d
KirillZholnerovich <danielcooverw@gmail.com>
2018-05-03 22:36:24 +0300

    Added new tests for MarketsInfoHelper.getTokenInfoFromMarkets method

--- core/src/test/java/io/rocketico/core/MarketsInfoHelperTest.kt
+++ core/src/test/java/io/rocketico/core/MarketsInfoHelperTest.kt
@@ -9,7 +9,24 @@ import org.junit.Assert.*
 class MarketsInfoHelperTest {
 
     @Test
-    fun getTokenInfoFromMarkets() {
-        println(MarketsInfoHelper.getTokenInfoFromMarkets(TokenType.ETH.codeName, Currency.USD.codeName))
+    fun getTokenInfoFromMarketsForEth() {
+        val info = MarketsInfoHelper.getTokenInfoFromMarkets(TokenType.ETH.codeName, Currency.USD.codeName)
+
+        assertTrue(info?.marketsInfo?.get(0)?.exchange != null)
+        assertTrue(info?.marketsInfo?.get(0)?.highestRate24h != null)
+        assertTrue(info?.marketsInfo?.get(0)?.lowestRate24h != null)
+        assertTrue(info?.marketsInfo?.get(0)?.marketCapitalization != null)
+        assertTrue(info?.marketsInfo?.get(0)?.traidingVolume24h != null)
+    }
+
+    @Test
+    fun getTokenInfoFromMarketsForWaBi() {
+        val info = MarketsInfoHelper.getTokenInfoFromMarkets(TokenType.WABI.codeName, Currency.USD.codeName)
+
+        assertTrue(info?.marketsInfo?.get(0)?.exchange != null)
+        assertTrue(info?.marketsInfo?.get(0)?.highestRate24h != null)
+        assertTrue(info?.marketsInfo?.get(0)?.lowestRate24h != null)
+        assertTrue(info?.marketsInfo?.get(0)?.marketCapitalization != null)
+        assertTrue(info?.marketsInfo?.get(0)?.traidingVolume24h != null)
     }
 }
