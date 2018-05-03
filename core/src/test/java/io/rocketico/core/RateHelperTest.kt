f7b90adb83de1869de8fd2c1883576f44b9cf393
uxname <uxname@gmail.com>
2018-05-03 13:16:36 +0300

    Refactored getTokenRateByDate test

--- core/src/test/java/io/rocketico/core/RateHelperTest.kt
+++ core/src/test/java/io/rocketico/core/RateHelperTest.kt
@@ -1,16 +1,21 @@
 package io.rocketico.core
 
 import io.rocketico.core.model.TokenType
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 import java.util.*
 
 class RateHelperTest {
 
     @Test
     fun getTokenRateByDate() {
-        val tmpRates = RateHelper.getTokenRateByDate(listOf(TokenType.ETH.codeName), Date(1525255200000L))
+        val tmpRates = RateHelper.getTokenRateByDate(
+                listOf(
+                        TokenType.ETH.codeName,
+                        TokenType.WABI.codeName
+                ),
+                Date(1525255200000) //GMT: Wednesday, May 2, 2018 10:00:00 AM
+        )
         assertEquals(1, tmpRates?.rates?.size)
     }
 
