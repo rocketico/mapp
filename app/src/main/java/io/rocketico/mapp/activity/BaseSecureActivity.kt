7728bf0b91677b18716873ed6239b35204e09677
KirillZholnerovich <danielcooverw@gmail.com>
2018-05-15 13:06:12 +0300

    Changed BaseSecureActivity

--- app/src/main/java/io/rocketico/mapp/activity/BaseSecureActivity.kt
+++ app/src/main/java/io/rocketico/mapp/activity/BaseSecureActivity.kt
@@ -4,16 +4,19 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.WindowManager
+import io.rocketico.mapp.BuildConfig
 
 @SuppressLint("Registered")
 open class BaseSecureActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //prevent show activity's thumbnail in recent apps
-        window.setFlags(
-                WindowManager.LayoutParams.FLAG_SECURE,
-                WindowManager.LayoutParams.FLAG_SECURE
-        );
+        if (!BuildConfig.DEBUG) {
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_SECURE,
+                    WindowManager.LayoutParams.FLAG_SECURE
+            )
+        }
         super.onCreate(savedInstanceState)
     }
 }
