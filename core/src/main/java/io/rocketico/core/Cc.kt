d2e40a2b8fddb838de9f507988d5c8e4631204f3
KirillZholnerovich <danielcooverw@gmail.com>
2018-05-21 20:51:42 +0300

    Changed server url

--- core/src/main/java/io/rocketico/core/Cc.kt
+++ core/src/main/java/io/rocketico/core/Cc.kt
@@ -2,5 +2,5 @@ package io.rocketico.core
 
 object Cc {
     const val HTTP_TIMEOUT = 1000 * 60 * 10 //10m
-    const val SERVER_URL = "http://134.17.25.175:8080/api/"
+    const val SERVER_URL = "http://176.9.73.197:8080/api/"
 }
